5fbb9e62c742adec57780a19a1f53aed089d800b
Vladimir Kobenyakov <vkobenyakov@gmail.com>
2023-06-09 00:15:53 +0300

    test push

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -2,10 +2,8 @@ from datetime import datetime
 
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.shortcuts import get_object_or_404
-
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
-
 from reviews.models import Category, Comment, Genre, Review, Title
 
 
